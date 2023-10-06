40c03105a2bb1818873e167586cc8af948efb73b
unknown <filip@siliconet.pl>
2023-10-06 16:26:53 +0200

    new project

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,24 @@
 ﻿namespace ConsoleApp1
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("wbisz dlugosc boku a");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("wbisz dlugosc boku b");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("wbisz dlugosc boku c");
+            int c = int.Parse(Console.ReadLine());
+
+            float p = (a + b + c) / 2;
+
+            double s = Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+
+
+            Console.WriteLine(s);
+
         }
     }
 }
